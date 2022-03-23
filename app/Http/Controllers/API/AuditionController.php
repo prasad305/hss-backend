12380d9e59887b617acbca745deafab01ff039dd
Monir Hossen <38731309+MonirHossen@users.noreply.github.com>
2022-03-23 18:20:31 +0600

    Some changes on Audition API

--- app/Http/Controllers/API/AuditionController.php
+++ app/Http/Controllers/API/AuditionController.php
@@ -11,6 +11,7 @@ use Intervention\Image\ImageManagerStatic as Image;
 use App\Models\Audition\AssignJudge;
 use Illuminate\Support\Facades\Validator;
 
+
 class AuditionController extends Controller
 {
 
@@ -19,7 +20,7 @@ class AuditionController extends Controller
         //
     }
 
-    public function adminStatus()
+    public function auditionAdminStatus()
     {
         $live = Audition::where([['admin_id', auth('sanctum')->user()->id], ['status', 1]])->count();
         $pending = Audition::where([['admin_id', auth('sanctum')->user()->id], ['status', 0]])->count();
@@ -31,7 +32,7 @@ class AuditionController extends Controller
     }
 
 
-    public function adminPendings()
+    public function auditionAdminPendings()
     {
         $pendings = Audition::where([['admin_id', auth('sanctum')->user()->id], ['status', 0]])->get();
         return response()->json([
@@ -58,23 +59,14 @@ class AuditionController extends Controller
 
     public function store(Request $request)
     {
-        
 
-        // return $request->star_ids;
-
-        // foreach ($array as $id){
-        //     return $id;
-        // }
-       
         $validator = Validator::make($request->all(), [
             'title' => 'required',
             'description' => 'required',
             'star_ids' => 'required',
         ]);
 
-        
-
-        
+        // return $request->all();
 
         if ($validator->fails()) {
             return response()->json([
@@ -85,6 +77,8 @@ class AuditionController extends Controller
             $audition = Audition::find($request->audition_id);
             $audition->title = $request->title;
             $audition->description = $request->description;
+            $audition->start_time = $request->start_time;
+            $audition->end_time = $request->end_time;
     
             if ($request->hasfile('banner')) {
                 $destination = $audition->banner;
@@ -99,6 +93,23 @@ class AuditionController extends Controller
                 Image::make($file)->resize(900, 400)->save($filename, 50);
     
                 $audition->banner = $filename;
+            }
+
+           
+
+            if ($request->hasFile('video')) {
+                $destination = $audition->video;
+
+                if (File::exists($destination)) {
+                    File::delete($destination);
+                }
+
+                $file = $request->file('video');
+                $folder_path = 'uploads/videos/auditions/';
+                $video_file_name = now()->timestamp . '.' . $file->getClientOriginalExtension();
+                // save to server
+                $request->video->move(public_path($folder_path), $video_file_name);
+                $audition->video = $folder_path . $video_file_name;
             }
     
     
@@ -138,14 +149,11 @@ class AuditionController extends Controller
     }
 
     public function getAudition($audition_id){
-        $audition = Audition::with('assignJudge')->find($audition_id);
-       
+        $audition = Audition::with('judge')->find($audition_id);
         $judge_ids = [];
-        foreach ($audition->assignJudge as $key => $judge) {
-           array_push($judge_ids, $judge->judge_id);
+        foreach ($audition->judge as $key => $star) {
+           array_push($judge_ids, $star->judge_id);
         }
-        
-        // return $judge_ids;
         return response()->json([
             'status' => 200,
             'audition' => $audition,
