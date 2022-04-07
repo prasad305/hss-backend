508fa4433b5ad2f47af972ff6e13fe10cbee6942
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-04-07 11:07:46 +0600

    api update & reboot add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
+    }
+
+    public function reboot()
+    {
+        Artisan::call('cache:clear');
+        Artisan::call('route:clear');
+        Artisan::call('view:clear');
+        file_put_contents(storage_path('logs/laravel.log'), '');
+        Artisan::call('key:generate');
+        Artisan::call('config:cache');
+        return '<center><h1>System Rebooted!</h1></center>';
     }
 }
