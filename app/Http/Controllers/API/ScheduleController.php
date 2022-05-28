dc1b5fcae52f1ba3da6f2cc9f28bc01890900956
Monir Hossen <38731309+MonirHossen@users.noreply.github.com>
2022-05-28 17:55:10 +0600

    schedule dynamic done

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\API;
+
 use Carbon\Carbon;
 
 use App\Http\Controllers\Controller;
@@ -13,52 +14,54 @@ class ScheduleController extends Controller
     //
     public function add_schedule(Request $request)
     {
+        // return $request->all();
 
-      
-        // $deleteable_ids = [];
-        //  return gettype($request->{'0'}['date']);
+        $undeleteable_ids = [];
         foreach ($request->all() as $key => $req) {
-            $star = User::where('parent_user',auth()->user()->id)->first();
-            if (isset($req['id'])) {
-                // return 'id has found';
-
-                // Schedule::whereNotIn('id',$req['id'])->where('date',$request->{'0'}['date'])->delete();
-
-            //    $pre_ids =  Schedule::where('date',$request->{'0'}['date'])->pluck('id');
-
-               
-
-                Schedule::where('id',$req['id'])->update([
-                    'event_type' => $req['event_type'],
-                    'from' => $req['from'],
-                    'to' => $req['to'],
-                    'date' => $req['date'],
-                ]);
-            }else{
-               
-                Schedule::insert([
-                    'admin_id' => auth('sanctum')->user()->id,
-                    'event_type' => $req['event_type'],
-                    'star_id' => $star->id,
-                    'from' => $req['from'],
-                    'to' => $req['to'],
-                    'date' => $req['date'],
-                    'month' => Carbon::parse($req['date'])->format('M'),
-                    'updated_at' => date("Y-m-d h:i:s"),
-                    'created_at' => date("Y-m-d h:i:s"),
-                ]);
+            if (isset($req['id'] ) && $req['id'] !=null) {
+                array_push($undeleteable_ids, $req['id']);
             }
-        
+        }
+        // delete previous schedule
+        Schedule::whereNotIn('id', $undeleteable_ids)->where('date', $request->{'0'}['date'])->delete();
+
+        foreach ($request->all() as $key => $req) {
+
+            if (isset($req['id'])) {
+                Schedule::where('id', $req['id'])->update([
+                    'event_type' => $req['event_type'],
+                    'from' => $req['from'],
+                    'to' => $req['to'],
+                    'date' => $req['date'],
+                ]);
+            } else {
+               
+                if ($req['from'] == null || $req['from'] == '' && $req['to'] == null || $req['to'] == '' && $req['event_type'] == null || $req['event_type'] == '') {
+                    return response()->json([
+                        'status' => 422,
+                        'message' => 'Please fill all the field and try again',
+                    ]);
+                } else {
+                    Schedule::insert([
+                        'admin_id' => auth('sanctum')->user()->id,
+                        'event_type' => $req['event_type'],
+                        'star_id' => auth('sanctum')->user()->star->id,
+                        'from' => $req['from'],
+                        'to' => $req['to'],
+                        'date' => $req['date'],
+                        'month' => Carbon::parse($req['date'])->format('M'),
+                        'updated_at' => date("Y-m-d h:i:s"),
+                        'created_at' => date("Y-m-d h:i:s"),
+                    ]); 
+                }
+            }
         }
 
         return response()->json([
-            'status'=>200,
+            'status' => 200,
             // 'result'=> $request->all(),
-            'message'=>'Schedule Added Successfully',
+            'message' => 'Schedule Added Successfully',
         ]);
-
-
-
     }
 
     public function selected_schedule(Request $request)
@@ -68,55 +71,55 @@ class ScheduleController extends Controller
 
         $date = [];
         foreach ($schedule as $key => $value) {
-            array_push($date,$value['date']);
+            array_push($date, $value['date']);
         }
 
 
         return response()->json([
-            'status'=>200,
-            'schedule'=> $date,
-            'message'=>'Added Successfully',
+            'status' => 200,
+            'schedule' => $date,
+            'message' => 'Added Successfully',
         ]);
-
     }
 
     public function dateWiseSchedule($date)
     {
-        $schedule = Schedule::where([['admin_id',auth('sanctum')->user()->id],['date',$date]])->get();
+        $schedule = Schedule::where([['admin_id', auth('sanctum')->user()->id], ['date', $date]])->get();
         return response()->json([
-            'status'=>200,
-            'schedule'=> $schedule,
+            'status' => 200,
+            'schedule' => $schedule,
         ]);
-
     }
 
     public function schedule_list()
     {
-        $schedule = Schedule::select('date','status')->distinct()->where('admin_id',auth('sanctum')->user()->id)->orderBy('date', 'DESC')->get();
-        
+        $schedule = Schedule::select('date', 'status')->distinct()->where('admin_id', auth('sanctum')->user()->id)->orderBy('date', 'DESC')->get();
+
         // $date = [];
         // foreach ($schedule as $key => $value) {
-            //     array_push($date,$value['date']);
-            // }
+        //     array_push($date,$value['date']);
+        // }
 
 
         return response()->json([
-            'status'=>200,
-            'schedule'=> $schedule,
-            'message'=>'Added Successfully',
+            'status' => 200,
+            'schedule' => $schedule,
+            'message' => 'Added Successfully',
         ]);
     }
 
     public function current_year_schedule_list()
     {
 
-        $schedule = Schedule::select(['event_type as title','date as startDate','from','to'])->whereBetween('date', 
-        [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->where('admin_id',auth('sanctum')->user()->id)->get();
+        $schedule = Schedule::select(['event_type as title', 'date as startDate', 'from', 'to'])->whereBetween(
+            'date',
+            [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]
+        )->where('admin_id', auth('sanctum')->user()->id)->get();
 
         return response()->json([
-            'status'=>200,
-            'schedule'=> $schedule,
-            'message'=>'Added Successfully',
+            'status' => 200,
+            'schedule' => $schedule,
+            'message' => 'Added Successfully',
         ]);
     }
 }
--- app/Http/Controllers/API/StarScheduleController.php
+++ app/Http/Controllers/API/StarScheduleController.php
@@ -14,48 +14,52 @@ class StarScheduleController extends Controller
     public function add_schedule(Request $request)
     {
 
-      
-        // $deleteable_ids = [];
-        //  return gettype($request->{'0'}['date']);
-        foreach ($request->all() as $key => $req) {
-            $star = User::where('parent_user',auth()->user()->id)->first();
-            if (isset($req['id'])) {
-                // return 'id has found';
-
-                // Schedule::whereNotIn('id',$req['id'])->where('date',$request->{'0'}['date'])->delete();
-
-            //    $pre_ids =  Schedule::where('date',$request->{'0'}['date'])->pluck('id');
-
-               
-
-                Schedule::where('id',$req['id'])->update([
-                    'event_type' => $req['event_type'],
-                    'from' => $req['from'],
-                    'to' => $req['to'],
-                    'date' => $req['date'],
-                ]);
-            }else{
-                Schedule::insert([
-                    'admin_id' => auth('sanctum')->user()->parent_user,
-                    'event_type' => $req['event_type'],
-                    'star_id' => auth()->user()->id,
-                    'from' => $req['from'],
-                    'to' => $req['to'],
-                    'date' => $req['date'],
-                    'month' => Carbon::parse($req['date'])->format('M'),
-                    'updated_at' => date("Y-m-d h:i:s"),
-                    'created_at' => date("Y-m-d h:i:s"),
-                ]);
-            }
         
+        $undeleteable_ids = [];
+        foreach ($request->all() as $key => $req) {
+            if (isset($req['id'] ) && $req['id'] !=null) {
+                array_push($undeleteable_ids, $req['id']);
+            }
+        }
+        // delete previous schedule
+        Schedule::whereNotIn('id', $undeleteable_ids)->where('date', $request->{'0'}['date'])->delete();
+
+        foreach ($request->all() as $key => $req) {
+
+            if (isset($req['id'])) {
+                Schedule::where('id', $req['id'])->update([
+                    'event_type' => $req['event_type'],
+                    'from' => $req['from'],
+                    'to' => $req['to'],
+                    'date' => $req['date'],
+                ]);
+            } else {
+               
+                if ($req['from'] == null || $req['from'] == '' && $req['to'] == null || $req['to'] == '' && $req['event_type'] == null || $req['event_type'] == '') {
+                    return response()->json([
+                        'status' => 422,
+                        'message' => 'Please fill all the field and try again',
+                    ]);
+                } else {
+                    $admin = User::where('parent_user',auth()->user()->parent_user)->first();
+                    Schedule::insert([
+                        'admin_id' => $admin->id,
+                        'event_type' => $req['event_type'],
+                        'star_id' => auth('sanctum')->user()->id,
+                        'from' => $req['from'],
+                        'to' => $req['to'],
+                        'date' => $req['date'],
+                        'month' => Carbon::parse($req['date'])->format('M'),
+                    ]); 
+                }
+            }
         }
 
         return response()->json([
-            'status'=>200,
+            'status' => 200,
             // 'result'=> $request->all(),
-            'message'=>'Schedule Added Successfully',
+            'message' => 'Schedule Added Successfully',
         ]);
-
 
 
     }
