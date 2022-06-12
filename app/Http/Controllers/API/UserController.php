6ed284ac9cb0decdbfe0bcf89071663db25d9a6a
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-06-12 18:59:07 +0600

    question & answer slot check api add

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -559,7 +559,6 @@ class UserController extends Controller
             $available_status = false;
         }
 
-
         return response()->json([
             'status' => 200,
             'start_time' => $user_start_time,
