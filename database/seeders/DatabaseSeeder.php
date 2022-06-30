16de98bb575805fa326d4bbc282d4e639fea6fce
Monir Hossen <38731309+MonirHossen@users.noreply.github.com>
2022-06-30 18:51:41 +0600

    round rules modification

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,17 +35,17 @@ class DatabaseSeeder extends Seeder
         $this->call(GreetingTypeSeeder::class);
 
         // audition related seeder
-        $this->call(Audition\AuditionSeeder::class);
-        $this->call(Audition\AuditionJudgeInstructionSeeder::class);
-        $this->call(Audition\AuditionUploadVideoSeeder::class);
-        $this->call(Audition\AuditionUserVotingSeeder::class);
-        $this->call(Audition\AuditionJudgeMarkSeeder::class);
-        $this->call(Audition\AuditionAssignJurySeeder::class);
+        // $this->call(Audition\AuditionSeeder::class);
+        // $this->call(Audition\AuditionJudgeInstructionSeeder::class);
+        // $this->call(Audition\AuditionUploadVideoSeeder::class);
+        // $this->call(Audition\AuditionUserVotingSeeder::class);
+        // $this->call(Audition\AuditionJudgeMarkSeeder::class);
+        // $this->call(Audition\AuditionAssignJurySeeder::class);
 
-        $this->call(Audition\AuditionRulesSeeder::class);
-        $this->call(Audition\AuditionRoundRulesSeeder::class);
-        $this->call(Audition\AuditionAssignJudgeSeeder::class);
-        $this->call(Audition\AuditionParticipantsSeeder::class);
+        // $this->call(Audition\AuditionRulesSeeder::class);
+        // $this->call(Audition\AuditionRoundRulesSeeder::class);
+        // $this->call(Audition\AuditionAssignJudgeSeeder::class);
+        // $this->call(Audition\AuditionParticipantsSeeder::class);
 
     }
 }
