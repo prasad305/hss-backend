d46ecea2e6c68b98331d126efc0e1afed58146fe
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-07-07 17:52:46 +0600

    approved video list show

--- app/Http/Controllers/API/Audition/Admin/AuditionController.php
+++ app/Http/Controllers/API/Audition/Admin/AuditionController.php
@@ -199,11 +199,20 @@ class AuditionController extends Controller
         })->where([['audition_id', $audition->id]])->get();
 
         $assignJuriesOrderByGroup = $assignJuries->groupBy('group_id');
+
+        $auditionParticipantWithVideos = AuditionParticipant::with(['videos' => function ($query) use ($audition_round_info_id) {
+            return $query->where([['round_info_id', $audition_round_info_id], ['approval_status', 1]])->get();
+        }, 'participant'])->where([['audition_id', $audition_id], ['round_info_id', $audition_round_info_id]])->get();
+
+        $totalNumberOfVideos = AuditionUploadVideo::where([['round_info_id', $audition_round_info_id],[$audition_id, 'audition_id'], ['approval_status', 1]])->count();
+
         return response()->json([
             'status' => 200,
             'audition' => $audition,
             'auditionRoundInfo' => $auditionRoundInfo,
             'assignJuriesOrderByGroup' => $assignJuriesOrderByGroup,
+            'auditionParticipantWithVideos' => $auditionParticipantWithVideos,
+            'totalNumberOfVideos' => $totalNumberOfVideos,
         ]);
     }
     public function singleAuditionInstruction($id)
