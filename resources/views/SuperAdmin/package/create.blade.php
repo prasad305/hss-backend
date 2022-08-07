7bb39d3048d7ccda54b103a51f7754337f9a8103
Partha Proteem Ghose <96822921+parthatfp@users.noreply.github.com>
2022-08-07 19:12:19 +0600

    wallet done

--- resources/views/SuperAdmin/package/create.blade.php
+++ resources/views/SuperAdmin/package/create.blade.php
@@ -2,7 +2,7 @@
     @csrf
 
     <div class="row">
-          <div class="form-group col-md-6">
+          <div class="form-group col-md-12">
             <label for="" class="form-label">Package Name</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Package name">
           </div>
@@ -30,15 +30,57 @@
             <label for="" class="form-label">Greetings</label>
             <input type="text" class="form-control" id="greetings" value="0" name="greetings" placeholder="Greetings value">
           </div>
+        <div class="form-group col-md-6">
+          <label for="" class="form-label">Q&A</label>
+          <input type="text" class="form-control" id="qna" name="qna" value="0" placeholder="Q & A value">
+        </div>
+        
           <div class="form-group col-md-6">
             <label for="" class="form-label">Price</label>
             <input type="text" class="form-control" id="price" name="price" placeholder="Price value">
           </div>
+
+          <!-- <div class="form-group col-md-6">
+            <label for="" class="form-label">Color Code</label>
+            <input type="text" class="form-control" id="color_code" value="#000" name="color_code" placeholder="Color Code value">
+          </div> -->
+
+          <div class="form-group col-md-12">
+          <label for="" class="form-label">Color Code</label>
+            <div class="input-group mb-2">
+              <div class="input-group-prepend">
+                <!-- <div class="input-group-text">@</div> -->
+                <input name="color" value="@" class="form-control" style="width: 60px;" type="color" id="get" onchange="fetch()"/><br />
+              </div>
+              <!-- <input type="text" class="form-control" id="put" placeholder="Username"> -->
+              <input name="color_code"  class="form-control" type="text" id="color_code"/>
+            </div>
+          </div>
+
+
+          <!-- <div class="form-group col-md-6">
+            <label for="color">Color pick: </label>
+            <input name="color" type="color" id="get" onchange="fetch()"/><br />
+            <label for="hexa">Hex Code: </label>
+            <input name="hexa" type="text" id="put"/><br />
+        </div> -->
+
+
+          
+
+
       </div>
 
     <button type="submit" id="addCategoryBtn" class="btn btn-primary"><i class="fa fa-save"></i>&nbsp; Add Package</button>
   </form>
 
+  <script>
+    function fetch()
+    {
+      var get = document.getElementById("get").value;
+      document.getElementById("color_code").value = get;
+    }
+  </script>
   <script>
     $(document).on('click','#addCategoryBtn',function (event) {
         event.preventDefault();
