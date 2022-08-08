9eb8685d54d6428ed716162cb2a8d1591bc7f669
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-08-08 11:32:14 +0600

    code optimize

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,10 +34,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(Audition\AuditionSeeder::class);
         // $this->call(Audition\AuditionJudgeInstructionSeeder::class);
         // $this->call(Audition\AuditionUploadVideoSeeder::class);
-        // $this->call(Audition\AuditionUserVotingSeeder::class);
-        // $this->call(Audition\AuditionJudgeMarkSeeder::class);
         // $this->call(Audition\AuditionAssignJurySeeder::class);
-
         // $this->call(Audition\AuditionRulesSeeder::class);
         // $this->call(Audition\AuditionRoundRulesSeeder::class);
         // $this->call(Audition\AuditionAssignJudgeSeeder::class);
