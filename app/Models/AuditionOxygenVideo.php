43b640bc01541fd3c856cb5da974d7b0313f4764
A. Kaiyum <akaiyum.cse@gmail.com>
2022-09-28 20:12:41 +0600

    oxygen video uploaded

--- app/Models/AuditionOxygenVideo.php
+++ app/Models/AuditionOxygenVideo.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Audition\AuditionRoundInfo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AuditionOxygenVideo extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $with = ['user'];
+
+    public function auditionRoundInfo()
+    {
+        return $this->belongsTo(AuditionRoundInfo::class, 'round_info_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
