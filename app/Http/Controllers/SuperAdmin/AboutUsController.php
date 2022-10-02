9b53cc57613a16203b6403c8b327da51a117d03c
Nusrat Bhuiyan <nusratdevo@gmail.com>
2022-10-02 15:10:38 +0600

    Landing Setting Page Migration

---
+++ app/Http/Controllers/SuperAdmin/AboutUsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller\SuperAdmin;
+use Illuminate\Http\Request;
+
+class AboutUsController extends Controller
+{
+    //
+}
