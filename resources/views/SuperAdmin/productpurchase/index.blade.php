db28007094ae885866093f35adda0826b26bd254
Ismailcse98 <ismailbdcse@gmail.com>
2022-10-02 20:01:43 +0600

    title change

--- resources/views/SuperAdmin/productpurchase/index.blade.php
+++ resources/views/SuperAdmin/productpurchase/index.blade.php
@@ -13,12 +13,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Auction</h1>
+          <h1 class="m-0">Product Purchase</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Auction</li>
+            <li class="breadcrumb-item active">Product Purchase</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
