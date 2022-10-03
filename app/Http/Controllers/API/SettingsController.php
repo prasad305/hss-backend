e46cb4472e855c76bedca651642d74eeafa13d8e
Ismailcse98 <ismailbdcse@gmail.com>
2022-10-03 11:08:52 +0600

    create api for user policy

---
+++ app/Http/Controllers/API/SettingsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\AboutUs;
+use App\Models\PrivacyPolicy;
+use App\Models\FAQ;
+use App\Models\ProductPurchase;
+use App\Models\TarmsCondition;
+use App\Models\RefundPolicy;
+
+class SettingsController extends Controller
+{
+    public function aboutus()
+    {
+        $data = AboutUs::orderBy('id', 'DESC')->first();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+    public function policy()
+    {
+        $data = PrivacyPolicy::orderBy('id', 'desc')->where('status', 1)->get();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+    public function faq()
+    {
+        $data = FAQ::where('status', 1)->orderBy('id', 'DESC')->get();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+    public function productPurchase()
+    {
+        $data = ProductPurchase::orderBy('id', 'DESC')->first();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+    public function termsCondition()
+    {
+        $data = TarmsCondition::orderBy('id', 'DESC')->first();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+    public function refund()
+    {
+        $data = RefundPolicy::orderBy('id', 'DESC')->first();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'Souvenir Added Successfully',
+        ]);
+    }
+}
