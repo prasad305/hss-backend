2e08a655040a551885597b6cf4901356cf2e78bc
Ismailcse98 <ismailbdcse@gmail.com>
2022-10-25 15:43:57 +0600

    update jury profile from SA

--- resources/views/SuperAdmin/jury/edit.blade.php
+++ resources/views/SuperAdmin/jury/edit.blade.php
@@ -4,44 +4,34 @@
     <div class="row form-group">
         <div class="col-md-6">
               <label for="first_name">First Name</label>
-              <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter Admin First Name" value="{{$admin->first_name}}">
+              <input type="text" class="form-control" id="first_name" name="first_name"  value="{{$jury->first_name}}">
          </div> 
          <div class="col-md-6">
               <label for="last_name">Last Name</label>
-              <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Admin Last Name" value="{{$admin->last_name}}">
+              <input type="text" class="form-control" id="last_name" name="last_name"  value="{{$jury->last_name}}">
         </div>
      </div>
 
     <div class="form-group row">
         <div class="col-md-6">
           <label for="phone">Phone</label>
-          <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter Admin Phone" value="{{$admin->phone}}">
+          <input type="text" class="form-control" id="phone" name="phone"  value="{{$jury->phone}}">
         </div>
         <div class="col-md-6">
              <label for="email">Email</label>
-             <input type="text" class="form-control" id="email" name="email" placeholder="Enter Admin Email" value="{{$admin->email}}">
-        </div>  
-    </div>
-    <div class="form-group row">
-        <div class="col-12">
-            <label for="name">Category</label>
-            <select name="category_id" id="category_id" class="form-control select2">
-                @foreach($categories as $data)
-                    <option @if($data->id == $admin->category_id) selected @endif value="{{ $data->id }}">{{ $data->name }}</option>
-                @endforeach
-            </select>
+             <input type="text" class="form-control" id="email" name="email" value="{{$jury->email}}">
         </div>  
     </div>
     <span class="row">
            <div class="form-group col-md-6">
               <label for="image">Image</label>
-              <br><img id="image1" onchange="validateMultipleImage('image1')" alt="icon" src="{{ asset($admin->image) }}" height="180px" width="180px" onerror="this.onerror=null;this.src='{{ asset(get_static_option('no_image')) }}';" required/>
+              <br><img id="image1" onchange="validateMultipleImage('image1')" alt="icon" src="{{ asset($jury->image) }}" height="180px" width="180px" onerror="this.onerror=null;this.src='{{ asset(get_static_option('no_image')) }}';" required/>
               <br><br>
               <input type="file" class="mt-2" id="image" name="image" onchange="document.getElementById('image1').src = window.URL.createObjectURL(this.files[0]); show(this)" accept=".jfif,.jpg,.jpeg,.png,.gif" required>
         </div>
         <div class="form-group col-md-6">
             <label for="image">Cover</label>
-            <br><img id="image2" onchange="validateMultipleImage('image2')" alt="icon" src="{{asset($admin->cover_photo)}}" height="180px" width="180px" onerror="this.onerror=null;this.src='{{ asset(get_static_option('no_image')) }}';" required/>
+            <br><img id="image2" onchange="validateMultipleImage('image2')" alt="icon" src="{{asset($jury->cover_photo)}}" height="180px" width="180px" onerror="this.onerror=null;this.src='{{ asset(get_static_option('no_image')) }}';" required/>
 
             <br><br>
             
@@ -50,7 +40,7 @@
       </div>
     </span>
 
-    <button type="submit" class="btn btn-primary" id="btnUpdateAdmin"><i class="fa fa-save"></i>&nbsp; Update Admin</button>
+    <button type="submit" class="btn btn-primary" id="btnUpdateAdmin"><i class="fa fa-save"></i>&nbsp; Update Jury</button>
 
 </form> 
 
@@ -69,7 +59,7 @@
     });
 
     $.ajax({
-        url: "{{ route('superAdmin.managerAdmin.update',$admin->id) }}",// your request url
+        url: "{{ route('superAdmin.jury.update',$jury->id) }}",// your request url
         data: formData,
         processData: false,
         contentType: false,
