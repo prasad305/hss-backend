e55f1c90632dbb786e680cada55a2c7054b5f054
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-05 16:11:48 +0600

    Update index.blade.php

--- resources/views/ManagerAdmin/LiveChat/index.blade.php
+++ resources/views/ManagerAdmin/LiveChat/index.blade.php
@@ -57,7 +57,7 @@
                                             Pending</a>
                                     @endif
 
-                                    <a href="{{ route('managerAdmin.meetupEvent.details', [$val->id]) }}" type="button"
+                                    <a href="{{ route('managerAdmin.liveChat.details', [$val->id]) }}" type="button"
                                         class="btn btnDetails waves-effect fw-bold waves-light">Details <i
                                             class="fa fa-angle-double-right"></i></a>
                                 </div>
@@ -68,43 +68,6 @@
                     <!--card end-->
                 @endforeach
             </div>
-
-
-            <div class="row">
-                @foreach ($events as $event)
-                    <!--card-->
-
-                    <div class="col-sm-6 col-lg-4 col-xl-3">
-                        <div class="card">
-                            <div class="panel panel-primary text-center">
-                                <div class="">
-                                    <img width="100%" src="{{ asset($event->banner) }}" alt="">
-                                </div>
-                                <div class="panel-body py-3">
-                                    <h3 class="text-ellipsis-line-1">{{ $event->title }}</h3>
-
-                                    @if ($event->status == 2)
-                                        <button type="button" class="btn btn-success waves-effect waves-light"><i
-                                                class="icon-checkmark-round"></i> Published</button>
-                                    @elseif($event->status < 2)
-                                        <a type="button" class="btn btn-warning waves-effect waves-light"><i
-                                                class="icon-record"></i>
-                                            Pending</a>
-                                    @endif
-
-                                    <a href="{{ route('managerAdmin.liveChat.details', [$event->id]) }}" type="button"
-                                        class="btn btn-info waves-effect waves-light">Details <i
-                                            class="fa fa-angle-double-right"></i></a>
-
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <!--card end-->
-                @endforeach
-            </div>
-
 
         </div> <!-- container -->
     </div> <!-- content -->
