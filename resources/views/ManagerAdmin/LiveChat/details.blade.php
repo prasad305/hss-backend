da0e41a83e958a451da7c803872bd7f05b55ed64
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-05 16:44:48 +0600

    Details | Live Chat

--- resources/views/ManagerAdmin/LiveChat/details.blade.php
+++ resources/views/ManagerAdmin/LiveChat/details.blade.php
@@ -79,116 +79,77 @@
 
                         </div>
                     </div>
-
                 </div>
 
-
-
-
-            </div>
-
-
-            <div class="row pt-5">
-
-                <div class="col-md-8 ">
-                    <div class="row card p-5">
+                <div class="col-md-12 mb-2">
+                    <div class=" card px-3 py-3">
                         <h3>{{ $event->title }}</h3>
-
-                        <h4><u>Description</u></h4>
-                        {!! $event->description !!}
-
-                        <h4><u>Instruction</u></h4>
-                        {!! $event->instruction !!}
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 card py-3">
-                            Event Date
-                            <h4 class="text-warning">{{ \Carbon\Carbon::parse($event->event_date)->format('d F,Y') }}
-                            </h4>
-                        </div>
-                        <div class="col-md-6 card py-3">
-                            Event Time
-                            <h4 class="text-warning">{{ \Carbon\Carbon::parse($event->start_time)->format('h:i A') }} -
-                                {{ \Carbon\Carbon::parse($event->end_time)->format('h:i A') }}</h4>
-                        </div>
+                        <div class="title-text text-warning mt-2">Description</div>
+                        <div class="description-text"> {!! $event->description !!}</div>
+                        <div class="title-text text-warning mt-2">Instruction</div>
+                        <div class="description-text"> {!! $event->instruction !!}</div>
                     </div>
                 </div>
 
-                <div class="col-md-4">
-                    <div class="card px-5 py-4">
-                        <div class="row">
-                            <div class="col-xs-6 content-center mr-2">
-                                @if ($event->star->image)
-                                    <img src="{{ asset($event->star->image) }}"
-                                        style="height: 80px; width: 80px; border-radius: 50%; border: 2px solid gray" />
-                                @else
-                                    <a href="{{ asset('demo_image/demo_user.png') }}" target="_blank">
-                                        <img src="{{ asset('demo_image/demo_user.png') }}" alt="Demo Image"
-                                            style="height: 80px; width: 80px; border-radius: 50%; border: 2px solid gray" />
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="col-xs-6">
-                                Star
-                                <h3>{{ $event->star->first_name }} {{ $event->star->last_name }}</h3>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-6 content-center mr-2">
-                                @if ($event->admin->image)
-                                    <img src="{{ asset($event->admin->image) }}" class="user_image"
-                                        style="height: 80px; width: 80px; border-radius: 50%; border: 2px solid gray"
-                                        alt="Image not found" />
-                                @else
-                                    <a href="{{ asset('demo_image/demo_user.png') }}" target="_blank">
-                                        <img src="{{ asset('demo_image/demo_user.png') }}" alt="Demo Image"
-                                            class="user_image"
-                                            style="height: 80px; width: 80px; border-radius: 50%; border: 2px solid gray" />
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="col-xs-6">
-                                Admin
-                                <h3>{{ $event->admin->first_name }} {{ $event->admin->last_name }}</h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card px-5 py-3">
-                        <h5>Registration: </h5>
-                        <hr>
-                        <div class="row">
-                            <div class="col-4">Registration Start:</div>
-                            <div class="col-8 text-warning">
-                                {{ \Carbon\Carbon::parse($event->registration_start_date)->format('d F,Y') }}</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-4">Registration End:</div>
-                            <div class="col-8 text-warning">
-                                {{ \Carbon\Carbon::parse($event->registration_end_date)->format('d F,Y') }}</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-4">Maximum Time:</div>
-                            <div class="col-8 text-warning">{{ $event->max_time }} Minute(s)</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-4">Minimum Time:</div>
-                            <div class="col-8 text-warning">{{ $event->min_time }} Minute(s)</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-4">Interval:</div>
-                            <div class="col-8 text-warning">{{ $event->interval }} Minute(s)</div>
-                        </div>
-
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Registration Start Date</label>
+                        <h4 class="text-warning">
+                            {{ \Carbon\Carbon::parse($event->registration_start_date)->format('d F,Y') }}</h4>
                     </div>
                 </div>
 
-            </div>
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Registration End Date</label>
+                        <h4 class="text-warning">
+                            {{ \Carbon\Carbon::parse($event->registration_end_date)->format('d F,Y') }}</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Minimum Time</label>
+                        <h4 class="text-warning">
+                            {{ $event->min_time }} Minute(s)</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Maximum Time</label>
+                        <h4 class="text-warning">
+                            {{ $event->max_time }} Minute(s)</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Interval</label>
+                        <h4 class="text-warning">
+                            {{ $event->interval }} Minute(s)</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Event Date</label>
+                        <h4 class="text-warning">
+                            {{ \Carbon\Carbon::parse($event->event_date)->format('d F,Y') }}</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3  mb-2">
+                    <div class="card px-3 p-2">
+                        <label for="Date">Event Time</label>
+                        <h4 class="text-warning">
+                            {{ \Carbon\Carbon::parse($event->start_time)->format('h:i A') }} -
+                            {{ \Carbon\Carbon::parse($event->end_time)->format('h:i A') }}</h4>
+                    </div>
+                </div>
 
 
-
-            @if ($event->status != 2)
-                <div class="card row">
+                <div class="card col-md-12 ">
                     <div class="card-header"
                         style="color: gold; letter-spacing: .01rem; font-size: 18px; border-bottom: 1px solid #000;">
                         Publish Post in News Feed
@@ -196,54 +157,59 @@
                     <div class="card-body">
                         <form action="{{ route('managerAdmin.liveChat.set_publish', [$event->id]) }}" method="post">
                             @csrf
-                            <div class="row">
-                                <div class="mb-3 col-md-6 col-6">
-                                    <label for="start_date" class="form-label">Post Start Date</label>
-                                    <input type="calender" class="form-control" id="datepicker"
-                                        style="background: coral; position: relative; padding-left: 33px;"
-                                        name="post_start_date" readonly="readonly" value="{{ old('post_start_date') }}" />
-                                    <i class="fa fa-calendar"
-                                        style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
-                                        aria-hidden="true"></i>
-                                    @if ($errors->has('post_start_date'))
-                                        <span class="text-danger">{{ $errors->first('post_start_date') }}</span>
-                                    @endif
+                            @if ($event->status != 2)
+                                <div class="row">
+                                    <div class="mb-3 col-md-3 mb-2">
+                                        <label for="start_date" class="form-label">Post Start Date</label>
+                                        <input type="calender" class="form-control input-post" id="datepicker"
+                                            name="post_start_date" readonly="readonly"
+                                            value="{{ old('post_start_date') }}" />
+                                        <i class="fa fa-calendar"
+                                            style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
+                                            aria-hidden="true"></i>
+                                        @if ($errors->has('post_start_date'))
+                                            <span class="text-danger">{{ $errors->first('post_start_date') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="mb-3 col-md-3 mb-2">
+                                        <label for="end_date" class="form-label">Post End Date</label>
+                                        <input type="text" class="form-control input-post" id="datepicker1"
+                                            name="post_end_date" readonly="readonly" value="{{ old('post_end_date') }}">
+                                        <i class="fa fa-calendar"
+                                            style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
+                                            aria-hidden="true"></i>
+                                        @if ($errors->has('post_end_date'))
+                                            <span class="text-danger">{{ $errors->first('post_end_date') }}</span>
+                                        @endif
+                                    </div>
                                 </div>
-                                <div class="mb-3 col-md-6 col-6">
-                                    <label for="end_date" class="form-label">Post End Date</label>
-                                    <input type="text" class="form-control" id="datepicker1"
-                                        style="background: coral; position: relative; padding-left: 33px;"
-                                        name="post_end_date" readonly="readonly" value="{{ old('post_end_date') }}">
-                                    <i class="fa fa-calendar"
-                                        style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
-                                        aria-hidden="true"></i>
-                                    @if ($errors->has('post_end_date'))
-                                        <span class="text-danger">{{ $errors->first('post_end_date') }}</span>
-                                    @endif
-                                </div>
-                            </div>
-
-
-                            <button type="submit" class="btn btn-outline-success mr-2" href="">Publish
-                                Now</button>
-                            <a type="button" class="btn btn-outline-warning px-5"
-                                onclick="Show('Edit Live Chat Event','{{ route('managerAdmin.liveChat.edit', $event->id) }}')">Edit</a>
-
-
+                                <button type="submit" class="btn btnPublish mr-2" href="">Publish
+                                    Now</button>
+                                <a type="button" class="btn btnEdit px-5"
+                                    onclick="Show('Edit Live Chat Event','{{ route('managerAdmin.liveChat.edit', $event->id) }}')">Edit</a>
+                            @endif
+                            @if ($event->status == 2)
+                                <form action="{{ route('managerAdmin.liveChat.set_publish', [$event->id]) }}"
+                                    method="post">
+                                    @csrf
+                                    <button type="submit" class="btn btnRemove mr-2 mt- mb-4">Remove From
+                                        Publish</button>
+                                </form>
+                            @endif
 
                         </form>
-
                     </div>
-
                 </div>
-            @endif
 
-            @if ($event->status == 2)
-                <form action="{{ route('managerAdmin.liveChat.set_publish', [$event->id]) }}" method="post">
-                    @csrf
-                    <button type="submit" class="btn btnRemove mr-2">Remove From Publish</button>
-                </form>
-            @endif
+
+
+            </div>
+
+
+
+
+
+
 
 
         </div>
--- resources/views/ManagerAdmin/MeetupEvents/details.blade.php
+++ resources/views/ManagerAdmin/MeetupEvents/details.blade.php
@@ -91,8 +91,6 @@
 
                 </div>
 
-
-
                 <div class="col-md-12 mb-2">
                     <div class=" card px-3 py-3">
                         <h3>{{ $meetup->title }}</h3>
@@ -132,8 +130,6 @@
                     </div>
                 </div>
 
-
-
                 <div class="col-md-3  mb-2">
                     <div class="card px-3 p-2">
                         <label for="Date">Event Date</label>
@@ -151,66 +147,65 @@
                     </div>
                 </div>
 
-            </div>
-
-
-            @if ($meetup->status != 2)
-                <div class="card row">
+                <div class="card col-md-12">
                     <div class="card-header"
                         style="color: gold; letter-spacing: .01rem; font-size: 18px; border-bottom: 1px solid #000;">
                         Publish Post in News Feed
                     </div>
                     <div class="card-body">
+
                         <form action="{{ route('managerAdmin.meetupEvent.set_publish', [$meetup->id]) }}" method="post">
                             @csrf
-                            <div class="row">
-                                <div class="mb-3 col-md-3 mb-2">
-                                    <label for="start_date" class="form-label">Post Start Date</label>
-                                    <input type="calender" class="form-control input-post" id="datepicker"
-                                        name="post_start_date" readonly="readonly" value="{{ old('post_start_date') }}" />
-                                    <i class="fa fa-calendar"
-                                        style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
-                                        aria-hidden="true"></i>
-                                    @if ($errors->has('post_start_date'))
-                                        <span class="text-danger">{{ $errors->first('post_start_date') }}</span>
-                                    @endif
+
+                            @if ($meetup->status != 2)
+                                <div class="row">
+                                    <div class="mb-3 col-md-3 mb-2">
+                                        <label for="start_date" class="form-label">Post Start Date</label>
+                                        <input type="calender" class="form-control input-post" id="datepicker"
+                                            name="post_start_date" readonly="readonly"
+                                            value="{{ old('post_start_date') }}" />
+                                        <i class="fa fa-calendar"
+                                            style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
+                                            aria-hidden="true"></i>
+                                        @if ($errors->has('post_start_date'))
+                                            <span class="text-danger">{{ $errors->first('post_start_date') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="mb-3 col-md-3 mb-2">
+                                        <label for="end_date" class="form-label">Post End Date</label>
+                                        <input type="text" class="form-control input-post" id="datepicker1"
+                                            name="post_end_date" readonly="readonly" value="{{ old('post_end_date') }}">
+                                        <i class="fa fa-calendar"
+                                            style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
+                                            aria-hidden="true"></i>
+                                        @if ($errors->has('post_end_date'))
+                                            <span class="text-danger">{{ $errors->first('post_end_date') }}</span>
+                                        @endif
+                                    </div>
                                 </div>
-                                <div class="mb-3 col-md-3 mb-2">
-                                    <label for="end_date" class="form-label">Post End Date</label>
-                                    <input type="text" class="form-control input-post" id="datepicker1"
-                                        name="post_end_date" readonly="readonly" value="{{ old('post_end_date') }}">
-                                    <i class="fa fa-calendar"
-                                        style="position: absolute; top: 41px; left: 18px; font-size: 20px;"
-                                        aria-hidden="true"></i>
-                                    @if ($errors->has('post_end_date'))
-                                        <span class="text-danger">{{ $errors->first('post_end_date') }}</span>
-                                    @endif
-                                </div>
-                            </div>
 
 
-                            <button type="submit" class="btn btnPublish  mr-2" href="">Publish
-                                Now</button>
-                            <a type="button" class="btn btnEdit px-5"
-                                onclick="Show('Edit Live Chat Event','{{ route('managerAdmin.meetupEvent.edit', $meetup->id) }}')">Edit</a>
+                                <button type="submit" class="btn btnPublish  mr-2" href="">Publish
+                                    Now</button>
+                                <a type="button" class="btn btnEdit px-5"
+                                    onclick="Show('Edit Live Chat Event','{{ route('managerAdmin.meetupEvent.edit', $meetup->id) }}')">Edit</a>
+                            @endif
 
-
+                            @if ($meetup->status == 2)
+                                <form action="{{ route('managerAdmin.meetupEvent.set_publish', [$meetup->id]) }}"
+                                    method="post">
+                                    @csrf
+                                    <button type="submit" class="btn btnRemove mr-2 mt- mb-4">Remove From Publish</button>
+                                </form>
+                            @endif
 
                         </form>
 
                     </div>
 
                 </div>
-            @endif
 
-            @if ($meetup->status == 2)
-                <form action="{{ route('managerAdmin.meetupEvent.set_publish', [$meetup->id]) }}" method="post">
-                    @csrf
-                    <button type="submit" class="btn btnRemove mr-2 mb-4">Remove From Publish</button>
-                </form>
-            @endif
-
-
+            </div>
         </div>
     </div>
 
