de5fa1278608bf20902097db5563bb31dbf9a03f
Ismailcse98 <ismailbdcse@gmail.com>
2022-12-18 17:45:22 +0600

    Update mail view

--- resources/views/Others/MailView/postView.blade.php
+++ resources/views/Others/MailView/postView.blade.php
@@ -12,69 +12,6 @@
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
-</head>
-
-<body marginheight="0" topmargin="0" marginwidth="0" leftmargin="0" class="main-mail">
-     <br> <br> <br> <br>
-     <!--100% body table-->
-     <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" class="body-email pt-5">
-          <tr>
-               <td style="height:20px;">&nbsp;</td>
-          </tr>
-
-          <tr>
-               <td class="text-center">
-                    <a href="https://www.hellosuperstars.com" title="logo" target="_blank" class="a-tag">
-                         <img width="80"
-                              src="https://www.hellosuperstars.com/static/media/HelloSuperStarLogo.b2954c0f9e2cfe31a215.png"
-                              title="logo" alt="logo">
-                    </a>
-               </td>
-          </tr>
-          <tr>
-
-               <td style="padding:0 35px;padding-top:20px">
-                    <h2 class="mail-cat text-center">
-                         {{$senderInfo->category->name}}
-                    </h2>
-
-                    <br>
-                    <p class="mail-title">
-                         Subject : {{$postInfo->title}}
-                    </p>
-                    <p class="mail-sir">Respected (Sir/Madam),</p>
-                    <p class="mail-description">
-                         {!! $postInfo->description !!}
-                    </p>
-
-
-               </td>
-          </tr>
-          <tr class="text-center">
-               <td>
-                    <div href="javascript:void(0);" class="top-footer text-center">
-
-                         POSTED BY {{$senderInfo->first_name}} {{$senderInfo->last_name}},
-                         <br>
-                         THANK YOU
-                         <br>
-                         {{ date("F j, Y, g:i a") }}
-                    </div>
-               </td>
-          </tr>
-          <tr>
-               <td style="height:20px;">&nbsp;</td>
-          </tr>
-
-          <td class="text-center">
-               <p class="mail-footer">
-                    &copy; <strong>www.hellosuperstars.com</strong></p>
-          </td>
-     </table>
-     <!--/100% body table-->
-     <br>
-
-
      <style type="text/css">
           @import url('https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap');
 
@@ -141,6 +78,68 @@
                line-height: 18px;
           }
      </style>
+</head>
+
+<body marginheight="0" topmargin="0" marginwidth="0" leftmargin="0" class="main-mail">
+     <br> <br> <br> <br>
+     <!--100% body table-->
+     <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" class="body-email pt-5">
+          <tr>
+               <td style="height:20px;">&nbsp;</td>
+          </tr>
+
+          <tr>
+               <td class="text-center">
+                    <a href="https://www.hellosuperstars.com" title="logo" target="_blank" class="a-tag">
+                         <img width="80"
+                              src="https://www.hellosuperstars.com/static/media/HelloSuperStarLogo.b2954c0f9e2cfe31a215.png"
+                              title="logo" alt="logo">
+                    </a>
+               </td>
+          </tr>
+          <tr>
+
+               <td style="padding:0 35px;padding-top:20px">
+                    <h2 class="mail-cat text-center">
+                         {{$senderInfo->category->name}}
+                    </h2>
+
+                    <br>
+                    <p class="mail-title">
+                         Subject : {{$postInfo->title}}
+                    </p>
+                    <p class="mail-sir">Respected (Sir/Madam),</p>
+                    <p class="mail-description">
+                         {!! $postInfo->description !!}
+                    </p>
+
+
+               </td>
+          </tr>
+          <tr class="text-center">
+               <td>
+                    <div href="javascript:void(0);" class="top-footer text-center">
+
+                         POSTED BY {{$senderInfo->first_name}} {{$senderInfo->last_name}},
+                         <br>
+                         THANK YOU
+                         <br>
+                         {{ date("F j, Y, g:i a") }}
+                    </div>
+               </td>
+          </tr>
+          <tr>
+               <td style="height:20px;">&nbsp;</td>
+          </tr>
+
+          <td class="text-center">
+               <p class="mail-footer">
+                    &copy; <strong>www.hellosuperstars.com</strong></p>
+          </td>
+     </table>
+     <!--/100% body table-->
+     <br>
+
 </body>
 
 </html>
